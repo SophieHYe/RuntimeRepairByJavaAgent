ede7c408c16f3bdfd135c8c2fe8183c464fb4a41
SophieHYe <yehe@rfchina.com>
2017-08-17 18:26:24 +0800

    update path of code

--- bug_fix_project/src/repair/java/agent/AttachAPI.java
+++ bug_fix_project/src/repair/java/agent/AttachAPI.java
@@ -19,14 +19,14 @@ public class AttachAPI {
 				processId = args[0];
 				agentJarPath = args[1];
 			} else{
-				processId="20176";
+				processId="1892";
 				agentJarPath = "D:\\current_project\\JavaAgent\\RuntimeRepairByJavaAgent\\agent_project\\target\\AgentProject-0.1.jar";
 			}
 			
 			
 			System.out.println("agentJarPath is: "+agentJarPath);
 			VirtualMachine virtualMachine = VirtualMachine.attach(processId);
-			String patchPath =  "D:\\current_project\\JavaAgent\\RuntimeRepairByJavaAgent\\bug_fix_project\\target\\classes\\repair\\java\\agentDividedByZero.class";
+			String patchPath =  "D:\\current_project\\JavaAgent\\RuntimeRepairByJavaAgent\\bug_fix_project\\target\\classes\\repair\\java\\agent\\DividedByZero.class";
 			virtualMachine.loadAgent(agentJarPath,patchPath);
 			System.out.println("the patch path is: "+patchPath);
 		} catch (Exception e) {
